38632797ba4a4be1b54d443363672728405bf19f
Hargun Kaur <hargunkaur286@gmail.com>
2025-07-17 02:08:01 +0530

    implementing signup endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,50 @@
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+// import { generateVerificationToken } from "../utils/generateVerificationToken.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
 export const signup = async (req, res) => {
-    res.send("signup route");
+    const {email, password, name} = req.body;
+    try{
+        if(!email || !password || !name){
+            throw new Error("All fields are required!");
+        }
+
+        const userAlreadyExists = await User.findOne({email});
+
+        console.log("userAlreadyExists", userAlreadyExists);
+
+        if(userAlreadyExists) {
+            return res.status(400).json({success: false, message: "User already exists!"});
+        }
+    }
+    catch(error){
+        res.status(400).json({success: false, message: error.message});
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(10000 + Math.random() * 900000).toString();;
+    const user = new User ({
+        email,
+        password: hashedPassword,
+        name,
+        verificationToken,
+        verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000 //24 hours
+    })
+
+    await user.save();
+
+    //jwt
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({
+        success: true,
+        message: "User created successfully!",
+        user: {
+            ...user._doc,
+            password: undefined,
+        },
+    });
 };
 
 export const login = async (req, res) => {
--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
